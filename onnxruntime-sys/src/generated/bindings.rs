ebb9dcb9b26ee681889b52b6db3b4f642b04a250
Nanashi <sevenc7c@sevenc7c.com>
2023-04-23 21:31:43 +0900

    Add: x86_64-linux-android用のバインディングを追加 (#21)
    * Add: x86_64-linux-android用のバインディングを追加
    
    * Update: bindings.rsを更新
    
    * Update: 1.40.0に更新
    
    * Update: workflowを更新
    
    * （ビルドテスト）
    
    * Fix: 構文修正
    
    * Add: build.rsにAndroid周りの処理を追加
    
    * Add: 存在確認を追加
    
    * Fix: 名前を修正
    
    * Fix: rustup target install忘れ

--- onnxruntime-sys/src/generated/bindings.rs
+++ onnxruntime-sys/src/generated/bindings.rs
@@ -10,6 +10,12 @@ include!(concat!(
     "/src/generated/linux/aarch64/bindings.rs"
 ));
 
+#[cfg(all(target_os = "android", target_arch = "x86_64"))]
+include!(concat!(
+    env!("CARGO_MANIFEST_DIR"),
+    "/src/generated/android/x86_64/bindings.rs"
+));
+
 #[cfg(all(target_os = "android", target_arch = "aarch64"))]
 include!(concat!(
     env!("CARGO_MANIFEST_DIR"),
